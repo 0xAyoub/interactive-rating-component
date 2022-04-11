845cae83016691ba76b840ddda1757f73e0c15f4
Ayoub Benouda <ayoubusiness1@gmail.com>
2022-04-11 13:41:09 +0200

    Dynamisation du sélécteur d'avis

---
+++ script.js
@@ -0,0 +1,17 @@
+const ratings = document.querySelectorAll('.bubble2')
+for (let i = 0; i < ratings.length; i++) {
+    ratings[i].addEventListener('click', function(){
+            try{
+                document.querySelector('.selected').classList.remove('selected')
+            } catch {}
+            this.classList.add('selected')  
+    })
+}
+
+
+
+
+
+
+
+
